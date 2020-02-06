90657c3e1c2a5b6ff14a5c0dec347229adde7c2c
Matheus Henrique <matherique@gmail.com>
2020-02-06 20:48:01 -0300

    add README.md and comments

---
+++ README.md
@@ -0,0 +1,9 @@
+# Fish Path
+
+## The problem
+
+I like the way that [fish shell](https://fishshell.com/) show the current working directory but I dont like the rest of it, so I did it to use in PS1 definition using "\$()"
+
+## Usage
+
+Just buidit with `go build` and add to PATH folder and use with "\$(binary-name)" instead "\w"
--- main.go
+++ main.go
@@ -9,28 +9,31 @@ import (
 )
 
 func main() {
+	// get file absolute path
 	fpa, err := filepath.Abs(".")
 
 	if err != nil {
 		log.Fatalf("could not get absolute file path: %v", err)
 	}
-
+	// remove home user dir path from fap
 	fromhome := strings.Replace(fpa, os.Getenv("HOME"), "", 1)
+	// divide by /
 	ss := strings.Split(fromhome, string(filepath.Separator))
 
 	var fc []string
-
+	// get only first char and add to slice
 	for _, p := range ss {
 		if p != "" {
 			fc = append(fc, p[0:1])
 		}
 
 	}
-
+	// join the result by /
 	result := strings.Join(fc, string(filepath.Separator))
+
 	if result != "" {
 		result = "/" + result
 	}
-
+	// put int stdout the result
 	fmt.Fprintf(os.Stdout, fmt.Sprintf("~%s", result))
 }
